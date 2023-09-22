254d859a5aa06d228d33fa295ef6eb941b3b74d1
hugofloresgarcia <hf01049@georgiasouthern.edu>
2023-09-22 15:34:59 -0500

    filling in process

--- Source/DeepLearning/WebModel.h
+++ Source/DeepLearning/WebModel.h
@@ -100,83 +100,83 @@ public:
         return false;
     }
 
-  // Load the output JSON and parse controls if needed (This step might need more detail based on your requirements)
-  juce::var controls = loadJsonFromFile(outputPath);
-  if (controls.isVoid()) {
-      DBG("Failed to load controls from JSON.");
-      return false;
-  }
-
-  // else, it should be a list of dicts
-  juce::Array<juce::var> *ctrlList = controls.getArray();
-  if (ctrlList == nullptr) {
-      DBG("Failed to load controls from JSON. ctrlList is null.");
-      return false;
-  }
-
-  // clear the m_ctrls vector
-  m_ctrls.clear();
-
-  // iterate through the list of controls
-  // and add them to the m_ctrls vector
-  for (int i = 0; i < ctrlList->size(); i++) {
-    juce::var ctrl = ctrlList->getReference(i);
-    if (!ctrl.isObject()) {
-        DBG("Failed to load controls from JSON. ctrl is not an object.");
+    // Load the output JSON and parse controls if needed (This step might need more detail based on your requirements)
+    juce::var controls = loadJsonFromFile(outputPath);
+    if (controls.isVoid()) {
+        DBG("Failed to load controls from JSON.");
         return false;
     }
-    
-    try{
-        // get the ctrl type
-        juce::String ctrl_type = ctrl["ctrl_type"].toString().toStdString();
 
-        // create the ctrl
-        if (ctrl_type == "slider") {
-          auto slider = std::make_shared<SliderCtrl>();
-          slider->id = juce::Uuid();
-          slider->label = ctrl["label"].toString().toStdString();
-          slider->minimum = ctrl["minimum"].toString().getFloatValue();
-          slider->maximum = ctrl["maximum"].toString().getFloatValue();
-          slider->step = ctrl["step"].toString().getFloatValue();
-          slider->value = ctrl["value"].toString().getFloatValue();
-
-          m_ctrls.push_back({slider->id, slider});
-          DBG("Slider: " + slider->label + " added");
-        }
-        else if (ctrl_type == "text") {
-          auto text = std::make_shared<TextBoxCtrl>();
-          text->label = ctrl["label"].toString().toStdString();
-          text->value = ctrl["value"].toString().toStdString();
-
-          m_ctrls.push_back({text->id, text});
-          DBG("Text: " + text->label + " added");
-        }
-        else if (ctrl_type == "audio_in") {
-          auto audio_in = std::make_shared<AudioInCtrl>();
-          audio_in->label = ctrl["label"].toString().toStdString();
-
-          m_ctrls.push_back({audio_in->id, audio_in});
-          DBG("Audio In: " + audio_in->label + " added");
-        }
-        else if (ctrl_type == "number_box") {
-          auto number_box = std::make_shared<NumberBoxCtrl>();
-          number_box->label = ctrl["label"].toString().toStdString();
-          number_box->min = ctrl["min"].toString().getFloatValue();
-          number_box->max = ctrl["max"].toString().getFloatValue();
-          number_box->value = ctrl["value"].toString().getFloatValue();
-          
-          m_ctrls.push_back({number_box->id, number_box});
-          DBG("Number Box: " + number_box->label + " added");
-        }
-        else {
-          DBG("failed to parse control with unknown type: " + ctrl_type);
-        }
-      }
-      catch (const char* e) {
-        DBG("Failed to load controls from JSON. " << e);
+    // else, it should be a list of dicts
+    juce::Array<juce::var> *ctrlList = controls.getArray();
+    if (ctrlList == nullptr) {
+        DBG("Failed to load controls from JSON. ctrlList is null.");
         return false;
-      }
     }
+
+    // clear the m_ctrls vector
+    m_ctrls.clear();
+
+    // iterate through the list of controls
+    // and add them to the m_ctrls vector
+    for (int i = 0; i < ctrlList->size(); i++) {
+      juce::var ctrl = ctrlList->getReference(i);
+      if (!ctrl.isObject()) {
+          DBG("Failed to load controls from JSON. ctrl is not an object.");
+          return false;
+      }
+      
+      try{
+          // get the ctrl type
+          juce::String ctrl_type = ctrl["ctrl_type"].toString().toStdString();
+
+          // create the ctrl
+          if (ctrl_type == "slider") {
+            auto slider = std::make_shared<SliderCtrl>();
+            slider->id = juce::Uuid();
+            slider->label = ctrl["label"].toString().toStdString();
+            slider->minimum = ctrl["minimum"].toString().getFloatValue();
+            slider->maximum = ctrl["maximum"].toString().getFloatValue();
+            slider->step = ctrl["step"].toString().getFloatValue();
+            slider->value = ctrl["value"].toString().getFloatValue();
+
+            m_ctrls.push_back({slider->id, slider});
+            DBG("Slider: " + slider->label + " added");
+          }
+          else if (ctrl_type == "text") {
+            auto text = std::make_shared<TextBoxCtrl>();
+            text->label = ctrl["label"].toString().toStdString();
+            text->value = ctrl["value"].toString().toStdString();
+
+            m_ctrls.push_back({text->id, text});
+            DBG("Text: " + text->label + " added");
+          }
+          else if (ctrl_type == "audio_in") {
+            auto audio_in = std::make_shared<AudioInCtrl>();
+            audio_in->label = ctrl["label"].toString().toStdString();
+
+            m_ctrls.push_back({audio_in->id, audio_in});
+            DBG("Audio In: " + audio_in->label + " added");
+          }
+          else if (ctrl_type == "number_box") {
+            auto number_box = std::make_shared<NumberBoxCtrl>();
+            number_box->label = ctrl["label"].toString().toStdString();
+            number_box->min = ctrl["min"].toString().getFloatValue();
+            number_box->max = ctrl["max"].toString().getFloatValue();
+            number_box->value = ctrl["value"].toString().getFloatValue();
+            
+            m_ctrls.push_back({number_box->id, number_box});
+            DBG("Number Box: " + number_box->label + " added");
+          }
+          else {
+            DBG("failed to parse control with unknown type: " + ctrl_type);
+          }
+        }
+        catch (const char* e) {
+          DBG("Failed to load controls from JSON. " << e);
+          return false;
+        }
+      }
 
     sendChangeMessage();
     m_loaded = true;
@@ -221,38 +221,33 @@ public:
             .getChildFile("ctrls.json");
 
       
+    juce::File scriptPath = juce::File::getSpecialLocation(
+      juce::File::currentApplicationFile
+    ).getChildFile("Contents/Resources/gradiojuce_client/gradiojuce_client");
 
+    std::string command = (
+      scriptPath.getFullPathName().toStdString() 
+      + " --mode process"
+      + " --url " + m_url 
+      + " --output_path " + tempOutputFile.getFullPathName().toStdString()
+      + " --ctrls_path " + tempCtrlsFile.getFullPathName().toStdString()
+    );
+    int result = std::system(command.c_str());
 
-    // std::string processCommand = "Resources/process --url " + m_url 
-    //                               + " --input_path " + tempFile.getFullPathName().toStdString()
-    //                               + " --ctrls_path " + ctrlsPath.getFullPathName().toStdString();
-    
-    // int processResult = std::system(processCommand.c_str());
-    // if (processResult != 0) {
-    //     DBG("Failed to run process script.");
-    //     return;
-    // }
+    if (result != 0) {
+        DBG("Failed to run process script.");
+        // return false;
+    }
 
+    // read the output file to a buffer
+    // TODO: the sample rate should not be the incoming sample rate, but
+    // rather the output sample rate of the daw?
+    load_buffer_from_file(tempOutputFile, *bufferToProcess, sampleRate);
 
-    // // TODO 
-    // // call the script in Resources/process
-    // // we will need to pass it three arguments:
-    // // --url <url>: the url of the gradio server
-    // // --input_path <path>: the path to the input file
-    // // --ctrls_path <path>: path to a json file with the control values including
-    // // the path to the input audio file. 
-
-
-
-    // // read the output file to a buffer
-    // // TODO: the sample rate should not be the incoming sample rate, but
-    // // rather the output sample rate of the daw?
-    // load_buffer_from_file(juce::File(output_audio_path), *bufferToProcess,
-    //                       sampleRate);
-
-    // // delete the temp input file
-    // tempFile.deleteFile();
-    // tempOutputFile.deleteFile();
+    // delete the temp input file
+    tempFile.deleteFile();
+    tempOutputFile.deleteFile();
+    tempCtrlsFile.deleteFile();
 
     return;
   }
