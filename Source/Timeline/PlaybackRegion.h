1137eabe0b22790f5693422ae6bc97a6143b7d5c
hugofloresgarcia <hf01049@georgiasouthern.edu>
2023-11-30 12:56:34 -0600

    remove example bloat

--- Source/Timeline/PlaybackRegion.h
+++ Source/Timeline/PlaybackRegion.h
@@ -45,7 +45,7 @@ public:
   // Event handlers
   void mouseDown(const juce::MouseEvent &m) override;
   void mouseUp(const juce::MouseEvent &) override;
-  void mouseDoubleClick(const juce::MouseEvent &) override;
+  // void mouseDoubleClick(const juce::MouseEvent &) override;
   void changeListenerCallback(juce::ChangeBroadcaster *) override;
 
   // ARA event handlers
