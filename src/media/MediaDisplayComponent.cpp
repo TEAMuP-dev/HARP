655c638f388ab1307e3725ee4e774a07aafaacbc
Nathan Pruyne <nathan.pruyne@gmail.com>
2024-10-28 01:22:42 -0500

    Update label width

--- src/media/MediaDisplayComponent.cpp
+++ src/media/MediaDisplayComponent.cpp
@@ -80,7 +80,7 @@ void MediaDisplayComponent::repositionLabelOverlays()
 
     float pixelsPerSecond = mediaWidth / visibleRange.getLength();
 
-    float minLabelWidth = 0.0015 * pixelsPerSecond;
+    float minLabelWidth = 0.1 * getMediaWidth();
     float maxLabelWidth = 0.10 * pixelsPerSecond;
 
     float contentWidth = getContentBounds().getWidth();
