aa80e334843b03825b9651c58d860c15fa954e1f
Derek LLanes <derek.llanes@gmail.com>
2026-04-08 12:42:50 -0400

    Implement Ticket 377 drum MIDI visualization

--- src/media/MidiDisplayComponent.cpp
+++ src/media/MidiDisplayComponent.cpp
@@ -96,10 +96,9 @@ void MidiDisplayComponent::loadMediaFile(const URL& filePath)
 
         int channel = midiMessage.getChannel();
 
-        if (channel == 10)
-        {
-            // TODO - handle drums channel
-        }
+        // Handle drums channel
+        bool isDrum = (channel == 10);
+
 
         if (midiMessage.isProgramChange())
         {
@@ -137,7 +136,8 @@ void MidiDisplayComponent::loadMediaFile(const URL& filePath)
                                   startTime,
                                   duration,
                                   static_cast<unsigned char>(velocity),
-                                  static_cast<unsigned char>(instrument));
+                                  static_cast<unsigned char>(instrument),
+                                  isDrum);
             pianoRoll.insertNote(n);
         }
     }
