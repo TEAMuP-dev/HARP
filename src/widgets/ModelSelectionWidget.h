af944b7f52af7f4d3b2c4a0e5d442d2366f434ae
Vansh Chugh <1shhmusic2002@gmail.com>
2026-06-22 16:41:14 -0500

    update links

--- src/widgets/ModelSelectionWidget.h
+++ src/widgets/ModelSelectionWidget.h
@@ -69,8 +69,8 @@ struct SharedChoices : public ChangeBroadcaster
         "teamup-tech/vampnet-conditional-music-generation",
         "teamup-tech/harmonic-percussive-separation",
         "teamup-tech/Kokoro-TTS",
-        "harp-dev/general-purpose-audio-effect-removal",
-        "harp-dev/generalizable-accompaniment-generation",
+        "teamup-tech/general-purpose-audio-effect-removal",
+        "teamup-tech/generalizable-accompaniment-generation",
         "teamup-tech/MegaTTS3-Voice-Cloning",
         "teamup-tech/midi-synthesizer",
         "teamup-tech/audioseal", // TODO - more intuitive name
