fb77014f9fad621cc0c94ca005c19b273aa46b7b
Vansh Chugh <1shhmusic2002@gmail.com>
2026-06-22 16:35:55 -0500

    Added AnyAccomp

--- src/widgets/ModelSelectionWidget.h
+++ src/widgets/ModelSelectionWidget.h
@@ -70,6 +70,7 @@ struct SharedChoices : public ChangeBroadcaster
         "teamup-tech/harmonic-percussive-separation",
         "teamup-tech/Kokoro-TTS",
         "harp-dev/general-purpose-audio-effect-removal",
+        "harp-dev/generalizable-accompaniment-generation",
         "teamup-tech/MegaTTS3-Voice-Cloning",
         "teamup-tech/midi-synthesizer",
         "teamup-tech/audioseal", // TODO - more intuitive name
